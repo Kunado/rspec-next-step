450e2f6d72e01f148918aa76925341516f2c079e
naoki saruta <katsusand1@gmail.com>
2020-02-28 06:51:34 +0900

    Remove it method's string arguments

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -2,16 +2,16 @@ require 'rubygems'
 require 'rspec'
 require './message_filter'
 
-describe MessageFilter do
+describe MessageFilter, 'with argument "foo"' do
   before do
     @filter = MessageFilter.new('foo')
   end
 
-  it 'should detect message with NG word' do
+  it {
     expect(@filter).to be_detect('hello from foo')
-  end
+  }
 
-  it 'should not detect message without NG word' do
+  it {
     expect(@filter).not_to be_detect('hello, world')
-  end
+  }
 end
