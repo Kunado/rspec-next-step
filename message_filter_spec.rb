4ccc80fa0f4244bf21826e9d1dae52bb87d9dbae
naoki saruta <katsusand1@gmail.com>
2020-02-27 20:27:33 +0900

    Add fake implementation of MessageFilter#detect? method

---
+++ message_filter_spec.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'rspec'
+require './message_filter'
+
+describe MessageFilter do
+  it 'should detect message with NG word' do
+    filter = MessageFilter.new('foo')
+    expect(filter.detect?('hello from foo')).to be true
+  end
+end
