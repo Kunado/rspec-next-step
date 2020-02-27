f55ec8a23e780c894e47f108bfa1b3ff07da3254
naoki saruta <katsusand1@gmail.com>
2020-02-27 20:49:43 +0900

    Replace matchers with predecate matchers

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -3,13 +3,15 @@ require 'rspec'
 require './message_filter'
 
 describe MessageFilter do
+  before do
+    @filter = MessageFilter.new('foo')
+  end
+
   it 'should detect message with NG word' do
-    filter = MessageFilter.new('foo')
-    expect(filter.detect?('hello from foo')).to be true
+    expect(@filter).to be_detect('hello from foo')
   end
 
   it 'should not detect message without NG word' do
-    filter = MessageFilter.new('foo')
-    expect(filter.detect?('hello, world!')).to be false
+    expect(@filter).not_to be_detect('hello, world')
   end
 end
