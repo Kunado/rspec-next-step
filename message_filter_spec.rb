b3f528f623f57dc59e6588f423f18b647932d7a7
naoki saruta <katsusand1@gmail.com>
2020-02-29 07:15:49 +0900

    Receive multiple words when initializing MessageFilter

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -8,3 +8,9 @@ describe MessageFilter, 'with argument "foo"' do
   it { is_expected.to be_detect('hello from foo') }
   it { is_expected.not_to be_detect('hello world') }
 end
+
+describe MessageFilter, 'with arguments "foo", "bar"' do
+  subject { MessageFilter.new('foo', 'bar') }
+
+  it { is_expected.to be_detect('hello from foo') }
+end
